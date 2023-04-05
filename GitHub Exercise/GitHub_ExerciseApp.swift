206e5073bd02193d0f08a92e109f7a8351360198
IDONTCRYQAQ <omgisgg@gmail.com>
2023-04-05 23:14:01 +0800

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by csapp sicp on 2023/4/5.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
